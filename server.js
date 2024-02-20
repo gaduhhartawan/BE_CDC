ab42dabb734bcef82cb5a56ef7512883744a3b04
Gaduh Hartawan <gaduhhartaone@gmail.com>
2024-02-20 16:05:55 +0700

    feat: update route and middleware

--- server.js
+++ server.js
@@ -4,12 +4,16 @@ const connectDB = require("./config/dbConn");
 
 // Route - Import
 const testRoute = require("./routes/test.route");
+const userRoute = require("./routes/user.route");
+const jobRoute = require("./routes/job.route");
 
 connectDB();
 const app = express();
 
 // Route
 app.use("/api", testRoute);
+app.use("/api/users", userRoute);
+app.use("/api/jobs", jobRoute);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
