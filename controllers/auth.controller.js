bd1a6a6e71b1113dd19dd0c102b2db1fd69d9f39
Gaduh Hartawan <gaduhhartaone@gmail.com>
2024-02-20 16:26:39 +0700

    feat: add isCompany attr to jwt

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -9,8 +9,8 @@ module.exports.Signup = async (req, res, next) => {
     if (existingUser) {
       return res.json({ message: "Email already exists" });
     }
-    const user = await User.create({ email, fullname, password , imgurl});
-    const token = createSecretToken(user._id);
+    const user = await User.create({ email, fullname, password, imgurl });
+    const token = createSecretToken(user._id, user.isCompany);
     res.cookie("token", token, {
       withCredentials: true,
       httpOnly: false,
@@ -25,27 +25,35 @@ module.exports.Signup = async (req, res, next) => {
 };
 
 module.exports.Login = async (req, res, next) => {
-    try {
-      const { email, password } = req.body;
-      if(!email || !password ){
-        return res.json({message:'All fields are required'})
-      }
-      const user = await User.findOne({ email });
-      if(!user){
-        return res.json({message:'Incorrect password or email' }) 
-      }
-      const auth = await bcrypt.compare(password,user.password)
-      if (!auth) {
-        return res.json({message:'Incorrect password or email' }) 
-      }
-       const token = createSecretToken(user._id);
-       res.cookie("token", token, {
-         withCredentials: true,
-         httpOnly: false,
-       });
-       res.status(201).json({ message: "User logged in successfully", success: true ,user_name: user.fullname, is_company: user.isCompany, img_url: user.imgUrl});
-       next()
-    } catch (error) {
-      console.error(error);
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.json({ message: "All fields are required" });
     }
-  };
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.json({ message: "Incorrect password or email" });
+    }
+    const auth = await bcrypt.compare(password, user.password);
+    if (!auth) {
+      return res.json({ message: "Incorrect password or email" });
+    }
+    const token = createSecretToken(user._id, user.isCompany);
+    res.cookie("token", token, {
+      withCredentials: true,
+      httpOnly: false,
+    });
+    res
+      .status(201)
+      .json({
+        message: "User logged in successfully",
+        success: true,
+        user_name: user.fullname,
+        is_company: user.isCompany,
+        img_url: user.imgUrl,
+      });
+    next();
+  } catch (error) {
+    console.error(error);
+  }
+};
