da3b355ddd00eee8bccc9287cb9584aa1af59ef3
Gaduh Hartawan <gaduhhartaone@gmail.com>
2024-02-21 22:25:10 +0700

    feat: add isAdmin to token

--- middlewares/verifyJwt.js
+++ middlewares/verifyJwt.js
@@ -5,9 +5,11 @@ const verifyToken = (req, res, next) => {
   if (!token) return res.status(401).json({ message: "Unauthorized" });
 
   jwt.verify(token, process.env.TOKEN_KEY, async (err, payload) => {
+    console.log(payload);
     if (err) return res.status(403).json({ message: "Token is invalid" });
     req.userId = payload.id;
     req.isCompany = payload.isCompany;
+    req.isAdmin = payload.isAdmin;
     next();
   });
 };
