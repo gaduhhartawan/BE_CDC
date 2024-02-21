b5a00513d76f6aef24023d837bd087b8d24d90de
Gaduh Hartawan <gaduhhartaone@gmail.com>
2024-02-21 22:27:32 +0700

    feat: update jwt verification

--- routes/job.route.js
+++ routes/job.route.js
@@ -10,12 +10,10 @@ const verifyToken = require("../middlewares/verifyJwt");
 
 const router = require("express").Router();
 
-router.use(verifyToken);
-
 router.get("/", getJobs);
 router.get("/:id", getJob);
-router.post("/", createJob);
-router.patch("/:id", updateJob);
-router.delete("/:id", deleteJob);
+router.post("/", verifyToken, createJob);
+router.patch("/:id", verifyToken, updateJob);
+router.delete("/:id", verifyToken, deleteJob);
 
 module.exports = router;
