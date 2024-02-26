90476c664222faf654772c22183ec11d7b51eb02
Gaduh Hartawan <gaduhhartaone@gmail.com>
2024-02-26 08:59:48 +0700

    feat: update token expiresIn

--- utils/SecretToken.js
+++ utils/SecretToken.js
@@ -3,6 +3,6 @@ const jwt = require("jsonwebtoken");
 
 module.exports.createSecretToken = (id, isCompany, isAdmin) => {
   return jwt.sign({ id, isCompany, isAdmin }, process.env.TOKEN_KEY, {
-    expiresIn: 3 * 24 * 60 * 60,
+    expiresIn: 3 * 24 * 60 * 60 * 1000,
   });
 };
