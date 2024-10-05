b70e28bcdb7a2edb6e6817b8806d90c8043835b6
gaduhhartawan <123380383+gaduhhartawan@users.noreply.github.com>
2024-10-05 21:45:24 +0700

    Update server.js

--- functions/server.js
+++ functions/server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 // Cors
 app.use(
   cors({
-    origin: ["http://localhost:8800", "http://localhost:5173"],
+    origin: ["http://localhost:8800", "http://localhost:5173", "https://careerpath-cdc.vercel.app/"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
   })
